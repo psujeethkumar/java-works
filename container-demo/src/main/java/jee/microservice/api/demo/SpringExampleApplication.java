38a4e54e62dd539012d26d08c7e29343930579de
Sujeeth Pakala <psujeethkumar@gmail.com>
2024-04-28 11:22:10 +0200

    Updated the package name

--- container-demo/src/main/java/jee/microservice/api/demo/SpringExampleApplication.java
+++ container-demo/src/main/java/jee/microservice/api/demo/SpringExampleApplication.java
@@ -1,9 +1,8 @@
-package com.javatechie.spring.openshift;
+package jee.microservice.api.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
